1a7eacd7e37b16bd7ffca3be9c7f8764e494ffe3
your username <vaziliybober@gmail.com>
2020-12-11 01:39:31 +0300

    set up environment

---
+++ webpack.config.cjs
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'source-map',
+  devServer: {
+    writeToDisk: (filePath) => {
+      return /\.css$/.test(filePath);
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
